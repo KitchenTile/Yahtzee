ae50cc1f57b7f729b59bcc0e633311c6d67686b1
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-19 11:34:18 +0100

    Add files via upload

--- src/components/DiceComponent.jsx
+++ src/components/DiceComponent.jsx
@@ -1,1 +1,19 @@
 import React, { useState } from "react";
+import "../styles/DiceStyles.css"
+
+
+const DiceComponent = ({ dice }) => {
+    return (
+        <div className="diceContainer">
+            {dice.map((value, index) => (
+                <div className="dice" key={index}>
+                    <h2>{index + 1}</h2>
+                    <h3>{value}</h3>
+                    
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default DiceComponent;
