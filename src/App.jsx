eb571ab46c362fca9bb5790fae41b565bdfb0e19
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-19 11:34:56 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
 import { useState } from 'react'
 import './App.css'
+import DiceComponent from './components/DiceComponent'
+import Game from './classes/game'
+import Player from './classes/player';
 
 function App() {
-  
+  const [game, setGame] = useState(new Game(new Player("Player1"), new Player("Player2")));
+  const [diceValue, setDiceValue] = useState(["","","","",""]);
+
+  const rollValue = () => {
+    setDiceValue(game.startTurn());
+    console.log(diceValue);
+  }
 
   return (
     <>
       <div>
-        <p>Yahtzee app!</p>
+        <h1>Yahtzee app!</h1>
+        <DiceComponent dice={diceValue}/>
+        <button onClick={rollValue}>Roll Dice</button>
       </div>
     </>
   )
 }
 
-export default App
+export default App;
