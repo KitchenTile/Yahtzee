2af7dccb6c0b0689f77cf94fc75cf34c6c3c7d5c
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-20 12:48:04 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,28 @@ import Player from './classes/player';
 function App() {
   const [game, setGame] = useState(new Game(new Player("Player1"), new Player("Player2")));
   const [diceValue, setDiceValue] = useState(["","","","",""]);
+  const [player, setPlayer] = useState(`${game.players[game.playersTurn].name}`);
+
 
   const rollValue = () => {
     setDiceValue(game.startTurn());
-    console.log(diceValue);
+    console.log(game.players)
   }
 
+  const endTurn = () => {
+    game.endTurn();
+    setDiceValue(["","","","",""]);
+    setPlayer(`${game.players[game.playersTurn].name}`)
+  }
+
+
   return (
-    <>
+    <> 
       <div>
-        <h1>Yahtzee app!</h1>
+        <h1>Yahtzee</h1>
+        <h1>{player}'s Turn</h1>
         <DiceComponent dice={diceValue}/>
-        <button onClick={rollValue}>Roll Dice</button>
+        {game.players[game.playersTurn].diceRollCount < 3 ? <button onClick={rollValue}> Roll Dice </button> : <button onClick={endTurn}> Finish Turn </button>}
       </div>
     </>
   )
