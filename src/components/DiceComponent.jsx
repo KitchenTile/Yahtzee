9d52ab01d61e5e8f58738531fa6b12984d482f42
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-21 23:30:46 +0100

    Add files via upload

--- src/components/DiceComponent.jsx
+++ src/components/DiceComponent.jsx
@@ -3,10 +3,22 @@ import "../styles/DiceStyles.css"
 
 
 const DiceComponent = ({ dice }) => {
+    const [isSelected, setIsSelected] = useState(false);
+    const [diceState, setDiceState] = useState("dice not-selected");
+    
+    const handleClick = () => {
+        if (isSelected) {
+            setDiceState("dice not-selected")
+        } else {
+            setDiceState("dice selected")
+        }
+        setIsSelected(!isSelected);
+    }
+
     return (
         <div className="diceContainer">
             {dice.map((value, index) => (
-                <div className="dice" key={index}>
+                <div className={diceState} id={index} onClick={handleClick}>
                     <h2>{index + 1}</h2>
                     <h3>{value}</h3>
                     
