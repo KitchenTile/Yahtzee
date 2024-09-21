7319c6bd4fddc9128d6e8502885f6d6276014de6
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-21 23:31:11 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import './App.css'
 import DiceComponent from './components/DiceComponent'
 import Game from './classes/game'
 import Player from './classes/player';
+import Scoreboard from './components/ScoreboardComponent';
 
 function App() {
-  const [game, setGame] = useState(new Game(new Player("Player1"), new Player("Player2")));
+  const [game, setGame] = useState(new Game(new Player("Player1"), new Player("Player2"))); //Might need to set the game to a new state to avoid this reseting the players state
   const [diceValue, setDiceValue] = useState(["","","","",""]);
   const [player, setPlayer] = useState(`${game.players[game.playersTurn].name}`);
+  const [score, setScore] = useState(0);
 
 
   const rollValue = () => {
     setDiceValue(game.startTurn());
-    console.log(game.players)
+    console.log(game.players);
+    game.players[game.playersTurn].scores[1].value += 1;
   }
 
   const endTurn = () => {
@@ -26,6 +29,7 @@ function App() {
     <> 
       <div>
         <h1>Yahtzee</h1>
+        <Scoreboard game={game}/>
         <h1>{player}'s Turn</h1>
         <DiceComponent dice={diceValue}/>
         {game.players[game.playersTurn].diceRollCount < 3 ? <button onClick={rollValue}> Roll Dice </button> : <button onClick={endTurn}> Finish Turn </button>}
