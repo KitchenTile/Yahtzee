0ac8971916fd67cfe7a53d0db6af6fe6ac476639
Blue <118444162+KitchenTile@users.noreply.github.com>
2024-09-21 23:30:22 +0100

    Add files via upload

--- src/components/ScoreboardComponent.jsx
+++ src/components/ScoreboardComponent.jsx
@@ -1,1 +1,24 @@
 import React, { useState } from "react";
+import "../styles/Scoreboard.css"
+
+
+const Scoreboard = ({game}) => {
+    return(
+        <div className="ScoreboardContainer">
+            {game.players.map((player, index) => (
+                <div key={index}>
+                    <h1>{player.name}'s Scoreboard</h1>
+                    <ul className="SbList" >
+                        {player.scores.map((score, scoreiIndex) => (
+                            <li className="SbListItem" key={scoreiIndex}>
+                                {score.key}{score.value}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            ))}
+        </div>
+    )
+}
+
+export default Scoreboard;
